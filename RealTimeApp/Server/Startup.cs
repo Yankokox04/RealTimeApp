223003bcac26a3c4e48810fa20d6fb98783d1549
Yanko Duran <yduran@assanet.com>
2023-01-23 16:15:40 -0400

    Add project files.

---
+++ RealTimeApp/Server/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RealTimeApp.Server {
+    public class Startup {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSignalR();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) { 
+            
+            }
+        }
+    }
+}
---
+++ RealTimeApp/Server/TextHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeApp.Server {
+    public class TextHub : Hub {
+        public async Task Send(string text) {
+            await Clients.All.SendAsync("ReceiveInformation",text);
+        }
+    }
+}
